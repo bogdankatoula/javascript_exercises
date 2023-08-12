dba6fd675c261ad8397ab1c04e12acc8ee72502e
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-12 15:28:25 +0200

    s3 extras ex3

--- S3/extra/ex3.js
+++ S3/extra/ex3.js
@@ -1,10 +1,10 @@
 const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const div$$ = document.querySelector("[data-function='printHere']");
 const ul$$ = document.createElement("ul");
-for (let i = 0; i < cars.length; i++){
-    const car = cars[i];
+div$$.appendChild(ul$$);
+for (const car of cars) {
     const li$$ = document.createElement("li");
     li$$.textContent = car;
     ul$$.appendChild(li$$);
 }
-const data$$ = document.querySelector('[data-function="printHere"]');
-data$$.appendChild(ul$$);
