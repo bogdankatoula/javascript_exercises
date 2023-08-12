83c7c7b3fa128a76a05a524a37dab3241a3ce5e8
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-12 16:54:07 +0200

    s3 extras ex4

--- S3/extra/ex4.js
+++ S3/extra/ex4.js
@@ -1,24 +1,29 @@
 const countries = [{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}];
 
 for (const country of countries) {
-    const div$$ = document.createElement("div");
-    const h4$$ = document.createElement("h4");
-    const img$$ = document.createElement("img");
-    h4$$.textContent = country.title;
-    img$$.setAttribute("src", country.imgUrl);
-    div$$.appendChild(h4$$);
-    div$$.appendChild(img$$);
-    document.body.appendChild(div$$);
-    const individualButton$$ = document.createElement("button");
-    div$$.appendChild(individualButton$$);
-    function erase () {
-        div$$.remove();
+const div$$ = document.createElement("div");
+document.body.appendChild(div$$); 
+const h4$$ = document.createElement("h4");
+div$$.appendChild(h4$$);
+h4$$.textContent = country.title;
+const img$$ = document.createElement("img");
+div$$.appendChild(img$$);
+img$$.setAttribute("src", country.imgUrl);
+// div$$.innerHTML = `<h4>${country.title}</h4><img src="${country.imgUrl}"/>`;
+const buttonRemove$$ = document.createElement("button");
+div$$.appendChild(buttonRemove$$);
+buttonRemove$$.textContent = "Eliminar";
+function removeElement () {
+    div$$.remove();
 }
-individualButton$$.addEventListener("click", erase);
+buttonRemove$$.addEventListener("click", removeElement);
+// button$$.addEventListener('click', function () {
+//     div$$.remove();
+// })
 }
 const button$$ = document.querySelector("button");
-function remover () {
-        const allDiv$$ = document.querySelectorAll('div');
-        allDiv$$[allDiv$$.length - 1].remove();
-    }
-button$$.addEventListener("click", remover);
+function removeLastElement (){
+    const allDiv$$ = document.querySelectorAll("div");
+    allDiv$$[allDiv$$.length-1].remove();
+}
+button$$.addEventListener("click", removeLastElement);
