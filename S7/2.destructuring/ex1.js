0307734ff83445a983463056728b78c46cdb6e5d
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-16 20:35:47 +0200

    destructuring

--- S7/2.destructuring/ex1.js
+++ S7/2.destructuring/ex1.js
@@ -1,5 +1,4 @@
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+
 const {title, gender, year} = game;
-console.log (title);
-console.log (gender);
-console.log (year);
+console.log(title, gender, year);
