2f3fc103a19ef6a487723035f3b4667f8e22d1b5
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-16 20:36:31 +0200

    cambios

--- S7/2.destructuring/ex4.js
+++ S7/2.destructuring/ex4.js
@@ -1,4 +1,4 @@
 const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
 const {name, itv} = car;
-const [year1, year2, year3] = itv;
-console.log (name, year1, year2, year3);
+const [year2015, year2011, year2020] = itv;
+console.log (name, year2015, year2011, year2020);
