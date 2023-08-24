957a602d1031827a9632ce236baa128f73d495b1
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-24 18:00:31 +0200

    pares

--- S7/5.filter/ex2.js
+++ S7/5.filter/ex2.js
@@ -1,3 +1,4 @@
 const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
-const edadPar = ages.filter ((age) => age%2 == 0);
-console.log (edadPar);
+
+const pares = ages.filter(par => par % 2 == 0);
+console.log (pares);
