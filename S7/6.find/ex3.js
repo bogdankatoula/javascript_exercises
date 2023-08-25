4941872a00bcbd88499abb02cd9e89fdb5ca5ff0
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-25 15:27:36 +0200

    found

--- S7/6.find/ex3.js
+++ S7/6.find/ex3.js
@@ -1,9 +1,9 @@
 const aliens = [{name: 'Zalamero', planet: 'Eden', age: 4029},{name: 'Paktu', planet: 'Andromeda', age: 32},{name: 'Cucushumushu', planet: 'Marte', age: 503021}];
 const mutations = [{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}];
 
-const alien = aliens.find(element=>element.name == "Cucushumushu");
-const mutation = mutations.find(element=>element.name == "Porompompero");
-console.log(alien, mutation);
+const foundAlien = aliens.find(element=>element.name == "Cucushumushu");
+const foundMutation = mutations.find(element=>element.name == "Porompompero");
+console.log(foundAlien, foundMutation);
 // Añadir la propiedad mutation al objeto alien
-alien.mutation = mutation;
-console.log(alien);
+foundAlien.mutation = foundMutation;
+console.log(foundAlien);
