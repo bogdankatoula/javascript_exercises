59aaa05549329e9805681a5ab92c9f3436a314d2
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-25 12:02:34 +0200

    filtered input

--- S7/5.filter/ex7.js
+++ S7/5.filter/ex7.js
@@ -1,15 +1,17 @@
 const streamers = [{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'}, {name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, {name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'}, {name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}];
 
-const handleChange = (event) => {
-    console.log(event);
-    const filteredStreamers = streamers.filter(streamer => streamer.name.toLowerCase().includes(input$$.value.toLowerCase()))
-    console.log(filteredStreamers);
-}
-
 const input$$ = document.querySelector('[data-function="toFilterStreamers"]');
-// const input$$ = document.querySelector('input');
 
-input$$.addEventListener('input', handleChange);
+const handleInput = ev => {
+    const filteredStreamer = streamers.filter(streamer => streamer.name.toLowerCase().includes(ev.target.value.trim().toLowerCase()));
+    
+    // otra forma mas larga
+    // const inputStreamer = ev.target.value.trim().toLowerCase();
+    // const filteredStreamer = streamers.filter(streamer => streamer.name.toLowerCase().includes(inputStreamer));
+    console.log(filteredStreamer);
+};
+input$$.addEventListener("input", handleInput);
+
 
 // otra forma parecida de hacerlo
 //primera funcion para poder filtrar  y poner la condicion
