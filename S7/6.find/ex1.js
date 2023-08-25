30ea2b5208d8efbd50976959c00d6bea74d936e2
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-25 15:25:11 +0200

    find y añadir propiedad

--- S7/6.find/ex1.js
+++ S7/6.find/ex1.js
@@ -1,3 +1,7 @@
 const numbers = [32, 21, 63, 95, 100, 67, 43];
-const number = numbers.find (function (element) {return element == 100})
-console.log (number);
+
+const number = numbers.find(element => element == 100);
+console.log(number);
+
+// const number = numbers.find (function (element) {return element == 100})
+// console.log (number);
--- S7/6.find/ex3.js
+++ S7/6.find/ex3.js
@@ -1,7 +1,9 @@
 const aliens = [{name: 'Zalamero', planet: 'Eden', age: 4029},{name: 'Paktu', planet: 'Andromeda', age: 32},{name: 'Cucushumushu', planet: 'Marte', age: 503021}];
 const mutations = [{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}];
 
-const busca = aliens.find (alien => alien.name === 'Cucushumushu');
-const busca2 = mutations.find (mutation => mutation.name === 'Porompompero');
-console.log (busca, busca2);
-busca.mutation = busca2
+const alien = aliens.find(element=>element.name == "Cucushumushu");
+const mutation = mutations.find(element=>element.name == "Porompompero");
+console.log(alien, mutation);
+// Añadir la propiedad mutation al objeto alien
+alien.mutation = mutation;
+console.log(alien);
